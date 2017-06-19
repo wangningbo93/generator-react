0db55a0dd29e4f3a0fe75a3f1e5263c31322ee84
wangningbo <wangningbo@58ganji.com>
2017-06-19 16:14:50 +0800

    React脚手架初始

---
+++ generators/app/templates/src/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Todos from './Todos/Todos';
+
+render(
+  <Todos />,
+  document.querySelector('#react')
+);
---
+++ generators/index.js
@@ -0,0 +1,36 @@
+var path = require("path");
+var chalk = require("chalk");
+var util = require("util");
+var yeoman = require("yeoman-generator");
+var yosay = require("yosay");
+
+var Reactpackage = yeoman.Base.extend({
+  info:function(){
+    this.log(chalk.green("I am going to build your app"));
+  },
+
+  generateBasic: function() {  //按照自己的templates目录自定义
+        this.directory('src', 'src');    //拷贝目录
+        this.copy('package.json', 'package.json');   //拷贝文件
+        this.copy('template.html', 'template.html');
+        this.copy('README.md', 'README.md');
+        this.copy('webpack.config.dev.js', 'webpack.config.dev.js');
+        this.copy('.babelrc','.babelrc');
+        this.copy('.gitignore','.gitignore');
+    },
+    generateClient: function() {
+        this.sourceRoot(path.join(__dirname, 'templates'));
+        this.destinationPath('./');
+    },
+    install: function() {      //安装依赖
+        this.installDependencies({
+            skipInstall: this.options['skip-install']
+        });
+    },
+    end: function() {
+        this.log(yosay(
+            'Your app has been created successfully!'
+        ));
+    }
+});
+module.exports = Reactpackage;
