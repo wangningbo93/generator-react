d071efae3e6e2687b569e2955a9f9fc95fad7e6a
wangningbo <wangningbo@58ganji.com>
2017-06-19 16:33:31 +0800

    react脚手架bug

--- generators/index.js
+++ generators/index.js
@@ -1,8 +1,8 @@
-var path = require("path");
-var chalk = require("chalk");
-var util = require("util");
-var yeoman = require("yeoman-generator");
-var yosay = require("yosay");
+var path = require('path');
+var chalk = require('chalk');    //不同颜色的info
+var util = require('util');
+var yeoman = require('yeoman-generator');
+var yosay = require('yosay');    //yeoman弹出框
 
 var Reactpackage = yeoman.Base.extend({
   info:function(){
@@ -10,27 +10,27 @@ var Reactpackage = yeoman.Base.extend({
   },
 
   generateBasic: function() {  //按照自己的templates目录自定义
-        this.directory('src', 'src');    //拷贝目录
-        this.copy('package.json', 'package.json');   //拷贝文件
-        this.copy('template.html', 'template.html');
-        this.copy('README.md', 'README.md');
-        this.copy('webpack.config.dev.js', 'webpack.config.dev.js');
-        this.copy('.babelrc','.babelrc');
-        this.copy('.gitignore','.gitignore');
-    },
-    generateClient: function() {
-        this.sourceRoot(path.join(__dirname, 'templates'));
-        this.destinationPath('./');
-    },
-    install: function() {      //安装依赖
-        this.installDependencies({
-            skipInstall: this.options['skip-install']
-        });
-    },
-    end: function() {
-        this.log(yosay(
-            'Your app has been created successfully!'
-        ));
-    }
+    this.directory('src', 'src');    //拷贝目录
+    this.copy('package.json', 'package.json');   //拷贝文件
+    this.copy('template.html', 'template.html');
+    this.copy('README.md', 'README.md');
+    this.copy('webpack.config.dev.js', 'webpack.config.dev.js');
+    this.copy('.babelrc','.babelrc');
+    this.copy('.gitignore','.gitignore');
+  },
+  generateClient: function() {
+      this.sourceRoot(path.join(__dirname, 'templates'));
+      this.destinationPath('./');
+  },
+  install: function() {      //安装依赖
+      this.installDependencies({
+          skipInstall: this.options['skip-install']
+      });
+  },
+  end: function() {
+    this.log(yosay(
+        'Your app has been created successfully!'
+    ));
+  }
 });
 module.exports = Reactpackage;
